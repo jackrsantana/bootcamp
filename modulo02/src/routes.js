e1b3448b6607bc4f8ca4a69b0da7c124bb69698c
Jackson Santana <jack.rsantana@gmail.com>
2019-07-12 13:06:00 -0300

    M2A10 - Criando loader de models

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello Jackson' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Jackson Santana',
+    email: 'jack.rsantana@gmail.com',
+    password_hash: '1238712387',
+  });
+
+  res.json(user);
 });
 
 export default routes;
