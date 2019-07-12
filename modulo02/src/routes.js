a231d8b3a3fcb280a26d466ab130fd48e4b46806
Jackson Santana <jack.rsantana@gmail.com>
2019-07-12 10:34:07 -0300

    M2A6 - ESLint, Prettier & EditorConfig

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  res.json({ message: "Hello Jackson" });
+routes.get('/', (req, res) => {
+  res.json({ message: 'Hello Jackson' });
 });
 
 export default routes;
