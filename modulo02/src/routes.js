3c5ef4efb9e96233b0a5465958fd97f7b04491c2
Jackson Santana <jack.rsantana@gmail.com>
2019-07-12 13:18:31 -0300

    M2A11 - Cadastro de usuários

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,16 +1,10 @@
 import { Router } from 'express';
 import User from './app/models/User';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Jackson Santana',
-    email: 'jack.rsantana@gmail.com',
-    password_hash: '1238712387',
-  });
-
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
