b7e255ef6064277cd04795980ab2cf2acf10a213
Jackson Santana <jack.rsantana@gmail.com>
2019-07-23 02:00:06 -0300

    M3A17 - Configurando fila com Redis

--- modulo03/src/app/controllers/AppointmentController.js
+++ modulo03/src/app/controllers/AppointmentController.js
@@ -1,13 +1,13 @@
 import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt-BR';
+
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
-
 import Notification from '../schemas/Notification';
-
-import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import CancellationMail from "../jobs/CancellationMail";
 
 class AppointmentController {
   async index(req, res) {
@@ -139,11 +139,11 @@ class AppointmentController {
       ],
     });
 
-    // if (appointment.user_id !== req.userId) {
-    //   return res.status(401).json({
-    //     error: "You don't have permission to cancel this appointment",
-    //   });
-    // }
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment",
+      });
+    }
 
     const dateWithSub = subHours(appointment.date, 2);
 
@@ -156,18 +156,7 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
-    });
+    Queue.add(CancellationMail.key, { appointment });
 
     return res.json(appointment);
   }
