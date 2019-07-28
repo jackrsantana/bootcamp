2100242b982a37d72e9096a4f5e66a4fc2d9642a
Jackson Santana <jack.rsantana@gmail.com>
2019-07-28 15:40:29 -0300

    M4A6 - Class Components

--- modulo04/src/App.js
+++ modulo04/src/App.js
@@ -1,15 +1,10 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import image from './assets/image.jpg'
+import "./App.css";
+import TechList from "./components/TechList";
 
 function App() {
-    return (
-        <div>
-            <h1>Hello World</h1>
-            <img src={image} width="200"></img>
-        </div>
-    );
+  return <TechList />;
 }
 
 export default App;
