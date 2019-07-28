54ac8b4f2a0de5d13feac041b312e490a74a752b
Jackson Santana <jack.rsantana@gmail.com>
2019-07-28 15:27:16 -0300

    M4A5 - Importando imagens

--- modulo04/src/App.js
+++ modulo04/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import './App.css';
+import image from './assets/image.jpg'
 
 function App() {
-    return <h1>Hello Jackson</h1>
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <img src={image} width="200"></img>
+        </div>
+    );
 }
 
 export default App;
--- modulo04/webpack.config.js
+++ modulo04/webpack.config.js
@@ -24,6 +24,9 @@ module.exports = {
                     { loader: 'style-loader' },
                     { loader: 'css-loader' }
                 ]
+            }, {
+                test: /.*\.*(gif|png|jpe?g)$/i,
+                use: {loader: 'file-loader'}
             }
         ]
     }
