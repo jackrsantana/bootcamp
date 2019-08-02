0fd856cfbb7cc9d966d13b132669391ed23f2074
Jackson Santana <jack.rsantana@gmail.com>
2019-08-02 16:39:48 -0300

    M6A5 - Estilos globais

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -5,14 +5,7 @@ import { Title } from './styles';
 export default function Main() {
   return (
     <>
-      <Title error>
-        Main
-        <small>Menor</small>
-      </Title>
-      <Title error={false}>
-        Main
-        <small>Menor</small>
-      </Title>
+      <Title>Hello World</Title>
     </>
   );
 }
