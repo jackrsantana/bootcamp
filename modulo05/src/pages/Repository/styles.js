d0f77cf27d8717f3fe618b9173b268a5ff25d08d
Jackson Santana <jack.rsantana@gmail.com>
2019-08-03 15:05:58 -0300

    M6A14 - Exibindo issues

--- modulo05/src/pages/Repository/styles.js
+++ modulo05/src/pages/Repository/styles.js
@@ -16,8 +16,8 @@ export const Owner = styled.header`
   align-items: center;
 
   a {
-    color: 7159c1;
-    fontsize: 16px;
+    color: #7159c1;
+    font-size: 16px;
     text-decoration: none;
   }
 
@@ -39,5 +39,65 @@ export const Owner = styled.header`
     line-height: 1.4;
     text-align: center;
     max-width: 400px;
+  }
+`;
+
+export const IssueList = styled.ul`
+  padding-top: 30px;
+  margin-top: 30px;
+  border-top: 1px solid #eee;
+  list-style: none;
+
+  li {
+    display: flex;
+    padding: 15px 10px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+
+    & + li {
+      margin-top: 10px;
+    }
+
+    img {
+      width: 36px;
+      height: 36px;
+      border-radius: 50%;
+      border: 2px solid #eee;
+    }
+
+    div {
+      flex: 1;
+      margin-left: 15px;
+
+      strong {
+        font-size: 16px;
+
+        a {
+          text-decoration: none;
+          color: #333;
+
+          &:hover {
+            color: #7159c1;
+          }
+        }
+
+        span {
+          background: #7159c1;
+          color: #fff;
+          border-radius: 4px;
+          font-size: 12px;
+          font-weight: 600;
+          height: 20px;
+          padding: 4px;
+          margin-left: 5px;
+        }
+      }
+
+      p {
+        margin-top: 5px;
+        font-size: 12px;
+        color: #999;
+      }
+    }
   }
 `;
