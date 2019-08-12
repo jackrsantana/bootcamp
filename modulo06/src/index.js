ea3867e08d45c2f3944fd17cb5d30357d03d0d89
jack_rsantana <jack.rsantana@gmail.com>
2019-08-12 18:24:43 -0300

    M6A5 - Configurando Reactotron

--- modulo06/src/index.js
+++ modulo06/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
+import './config/ReactotronConfig';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,7 +20,7 @@ const styles = StyleSheet.create({
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.welcome}>Welcome to React Natasdive!</Text>
     </View>
   );
 }
