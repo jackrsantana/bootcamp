30acffe729c43f3f3f954605432bb9ff91a2d876
jack_rsantana <jack.rsantana@gmail.com>
2019-08-12 22:37:42 -0300

    M6A14-Realizando navegação

--- modulo06/src/pages/User/index.js
+++ modulo06/src/pages/User/index.js
@@ -3,10 +3,8 @@ import { View } from 'react-native';
 
 // import { Container } from './styles';
 
-export default function Main() {
+export default function User({ navigation }) {
+  console.tron.log(navigation.getParam('user'));
+
   return <View />;
 }
-
-Main.navigationOptions = {
-  title: 'Usuários',
-};
